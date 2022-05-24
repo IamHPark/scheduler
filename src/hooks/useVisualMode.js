117c267dc5f0d45e14edee111a4c63780967f017
Heesoo <heesoo.p1023@gmail.com>
2022-05-23 19:00:31 -0700

    add hooks

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,5 +2,21 @@ import { useState } from "react"
 
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
-  return { mode };
+  const [history, setHistory] = useState([initial]);
+
+  const transition = (newMode, replace = false) => {
+    setMode(newMode)
+    setHistory( prev => replace ? [...prev.slice(0, prev.length-1), newMode] : [...prev, newMode]);
+  };
+
+  const back = () => {
+      console.log(history)
+      const historyCopy = [...history]
+      if(historyCopy.length > 1) { historyCopy.pop() }
+      setHistory(historyCopy)
+      const last = historyCopy.slice(-1)
+      setMode(...last)
+    }
+  return { mode, transition , back};
 }
+
